152008849eebe2339458d2667bb7881292862a6f
Van Darkholme <jija@kaavo>
2021-11-01 16:52:14 +0500

    Add project files.

---
+++ WrongQuestion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WrongQuestion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

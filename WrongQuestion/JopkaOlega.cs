6b05ac2fc1e5093fe1fcef17c2be7e45850f0722
Evgeniy <djonich@mail.ru>
2021-11-01 17:12:21 +0500

    ADDJOPA

---
+++ WrongQuestion/JopkaOlega.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WrongQuestion
+{
+    class JopkaOlega
+    {
+        private int depth { get; set; }
+        private string dirty { get; set; }
+
+        public JopkaOlega(int depth, string dirty)
+        {
+            this.depth = depth;
+            this.dirty = dirty;
+        }
+    }
+}

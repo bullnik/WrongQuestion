4d10b7aeddd6d85982675c12725bc20aeba83c30
Van Darkholme <jija@kaavo>
2021-11-01 16:57:26 +0500

    test jija

--- WrongQuestion/Program.cs
+++ WrongQuestion/Program.cs
@@ -7,6 +7,7 @@ namespace WrongQuestion
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("jija");
         }
     }
 }

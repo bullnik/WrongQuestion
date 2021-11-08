a02c9904c433b7503c25fe8d3966a49f65d2d17c
Van Darkholme <jija@kaavo>
2021-11-08 21:20:02 +0500

    add connent to tg

--- WrongQuestion/Program.cs
+++ WrongQuestion/Program.cs
@@ -1,14 +1,38 @@
 ﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Exceptions;
 
 namespace WrongQuestion
 {
     class Program
     {
-        static void Main(string[] args)
+        //Here is the token for bot Jijoba @jijoba_bot:
+        //2098827232:AAFu37Kco2dtw0vFRkNo0DYqKww68hY5Dh0
+        private static string Token => "2098827232:AAFu37Kco2dtw0vFRkNo0DYqKww68hY5Dh0";
+
+        private static TelegramBotClient Bot;
+
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("jija");
-            Console.WriteLine("Неправильный вопрос");
+            Bot = new TelegramBotClient(Token);
+
+            Bot.StartReceiving();
+
+            Bot.OnMessage += OnMessageHandler;
+            Console.ReadKey();
+
+            Bot.StopReceiving();
+        }
+
+        private static async void OnMessageHandler(object sender, MessageEventArgs e)
+        {
+            var msg = e.Message;
+            if (msg.Text != null)
+            {
+                Console.WriteLine(msg.Text);
+                await Bot.SendTextMessageAsync(msg.Chat.Id, "Ты написал: " + msg.Text + ". Пошел нахуй теперь.");
+            }
         }
     }
 }
